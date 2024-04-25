ab506621183a4bf1c5b6718202511e294e4df99d
Kit Law <klaw@opentable.com>
2024-04-24 20:55:11 -0600

    Bug fix OverdueBill

--- v_OverdueBill.sql
+++ v_OverdueBill.sql
@@ -1,4 +1,4 @@
--- VER 2024 Apr 18
+-- VER 2024 Apr 23
 
 ;DROP VIEW IF EXISTS OverdueBill
 GO
@@ -13,7 +13,7 @@ GO
     FROM ( SELECT refdate = (CASE WHEN Day(GetDate()) < 22 THEN DateAdd(Month, -1, GETDATE()) ELSE GETDATE() END) ) AS _
   ), d AS (
     SELECT
-        due_lv = DateDiff(Month, DateFromParts(ix_yrmo / 100, ix_yrmo % 100, 1), (SELECT tsmod FROM ix))
+        d_diff = DateDiff(Month, DateFromParts(ix_yrmo / 100, ix_yrmo % 100, 1), (SELECT tsmod FROM ix))
       , b.company, b.ix_yrmo, b.uid, b.cd_license, b.bill_type, b.create_dtm, b.amount, b.discount, b.bill_start, b.bill_end, b.due_dtm
       , cust_name = u.display_name, c.is_mobill, c.is_print_bill, c.is_rtn_envelope, c.balance
       , is_whatsapp = IsNull((SELECT TOP 1 1 FROM Contact WHERE uid = u.id AND c_type = 1 /*whataspp*/), 0)
@@ -25,22 +25,34 @@ GO
     JOIN TKUser AS u ON (b.uid = u.id)
     JOIN Customer AS c ON (b.uid = c.id)
     WHERE (
-          ( b.ix_yrmo = ( SELECT nxmo FROM ix ) AND c.is_mobill = 0 )
+           ( b.ix_yrmo = ( SELECT nxmo FROM ix ) AND c.is_mobill = 0 )
         OR ( b.ix_yrmo < ( SELECT nxmo FROM ix )                     )
     )
     -- due_dtm < DateAdd(MONTH, 2, DateFromParts(Year(GETDATE()), Month(GETDATE()), 1))
     -- ** due_dtm is useless due to client's requirement
+
+  ), DueLevel AS (
+    SELECT m.uid, m.cd_license, due_lv = (
+      CASE
+        WHEN max_diff > 2 THEN 2
+        WHEN max_diff < 0 THEN 0
+        ELSE max_diff -- 0 or 1
+      END)
+    FROM (
+      SELECT
+        d.uid, d.cd_license, max_diff = MAX(d_diff)
+      FROM d
+      GROUP BY d.uid, d.cd_license
+    ) AS m
   ), g AS (
     SELECT
-      due_lv = CASE WHEN d.due_lv > 2 THEN 2
-                    WHEN d.due_lv < 0 THEN 0
-                    ELSE d.due_lv
-              END,
-      company, ix_yrmo, uid, cd_license,
+      due_lv, company, ix_yrmo, d.uid, d.cd_license,
       bill_type, create_dtm, amount, discount, bill_start, bill_end,
       due_dtm, cust_name, is_mobill, is_print_bill, is_rtn_envelope, balance,
       is_whatsapp, is_email, is_mail, is_fax
     FROM d
+    LEFT JOIN DueLevel AS dl
+    ON (d.cd_license = dl.cd_license AND d.uid = dl.uid)
   )
   SELECT
       g.*, ca.*, sl.mgt_dis
@@ -64,7 +76,7 @@ GO
     FROM g
     GROUP BY uid
   ) AS vh
-  ON (g.uid = vh.owner_id)
+  ON (g.uid = vh.uid)
 GO
 
 
