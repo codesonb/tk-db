ac9d0f2cd01375a325bb2c65d2ab9c91a2ed53bd
Kit Law <klaw@opentable.com>
2024-05-05 16:26:37 -0600

    Add PrintConfig

--- PrintConfig.sql
+++ PrintConfig.sql
@@ -3,11 +3,11 @@ USE TKCar_UnitTest
 -- DROP TABLE PrintConfig;
 
 ;CREATE TABLE PrintConfig(
-  company    char(2)       NOT NULL
-, print_id   varchar(64)   NOT NULL
-, split      int           NOT NULL
-, background binary        NULL
-, fields     nvarchar(max) NOT NULL
+  company    char(2)        NOT NULL
+, print_id   varchar(64)    NOT NULL
+, split      int            NOT NULL
+, background varbinary(max) NULL
+, fields     nvarchar(max)  NOT NULL
 )
 
 ;ALTER TABLE PrintConfig ADD PRIMARY KEY(company, print_id)
